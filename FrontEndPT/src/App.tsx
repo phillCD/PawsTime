717baa128b8ef4b41dbd1783ab8a22e05c9aee09
Thiago Meller Casagrande <thiago.1359811@alunosatc.edu.br>
2024-03-07 20:26:22 -0300

    Front end first commit

---
+++ FrontEndPT/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SchedulePage from './pages/SchedulePage'
+
+function App() {
+
+  return (
+    <>
+      <SchedulePage />
+    </>
+  )
+}
+
+export default App
---
+++ FrontEndPT/src/pages/SchedulePage/index.tsx
@@ -0,0 +1,7 @@
+export default function SchedulePage() {
+    return(
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
