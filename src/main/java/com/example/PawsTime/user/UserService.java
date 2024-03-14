4bb9e3e402c6760a02c73c48ba112a8d77d80bc5
phillCD <fil.milaneze@gmail.com>
2024-03-14 20:16:25 -0300

    main: initialized UserService and its API

--- src/main/java/com/example/PawsTime/user/UserService.java
+++ src/main/java/com/example/PawsTime/user/UserService.java
@@ -1,4 +1,27 @@
 package com.example.PawsTime.user;
 
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+    private ModelMapper modelMapper;
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    /*public User createUser(UserRepresentation.UserCreate create) {
+        return userRepository.save(new User(
+        ));
+    }*/
 }
