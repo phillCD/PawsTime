d1d200a043947b0241ccc48dec60e6698e48717e
Thiago Meller Casagrande <thiago.1359811@alunosatc.edu.br>
2024-03-14 20:18:17 -0300

    Calendar

--- FrontEndPT/src/pages/SchedulePage/index.tsx
+++ FrontEndPT/src/pages/SchedulePage/index.tsx
@@ -1,10 +1,24 @@
-import Navbar from '../../components/Navbar';
+import { useState } from 'react';
 import "../../index.css"
+import Calendar from 'react-calendar';
+import { Value } from '../../@types/types';
+import 'react-calendar/dist/Calendar.css';
 
 export default function SchedulePage() {
-    return(
-        <div>
-            <Navbar />
+  const [value, onChange] = useState<Value>(new Date());
+
+
+  return(
+    <div className='h-screen p-8 bg-slate-100'>
+      <Calendar 
+          onChange={onChange} 
+          value={value}
+          locale="pt-BR"
+          formatShortWeekday={(_, date) => ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'][date.getDay()]}
+      />
+        <div className='bg-white '>
+
         </div>
-    )
+    </div>
+  )
 }
