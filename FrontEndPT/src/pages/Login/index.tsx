e8e9b2f7637f22762b3fc07ffb702cf18528cab5
Thiago Meller <thiagomellercasagrande@gmail.com>
2024-04-18 01:02:00 -0300

    login

--- FrontEndPT/src/pages/Login/index.tsx
+++ FrontEndPT/src/pages/Login/index.tsx
@@ -1,10 +1,7 @@
 export default function Login() {
-    return (
-        <div className="flex h-screen p-8 bg-green-500 gap-5 items-center justify-center">
-            <div className="bg-white w-96 h-96 rounded-md">
-                
-            </div>
-        </div>
-    );
-  }
-  
+  return (
+    <div className="flex h-screen p-8 bg-green-500 gap-5 items-center justify-center">
+      <div className="bg-white w-5/12 h-4/6 rounded-md"></div>
+    </div>
+  );
+}
