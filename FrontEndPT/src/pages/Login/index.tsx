a82cc1b50a7d58ee3f01137a522061cd95b444d4
Thiago Meller <thiagomellercasagrande@gmail.com>
2024-06-27 18:31:14 -0300

    loclalstotre

--- FrontEndPT/src/pages/Login/index.tsx
+++ FrontEndPT/src/pages/Login/index.tsx
@@ -9,21 +9,23 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleLogin = async (e: { preventDefault: () => void; }) => {
+  const handleLogin = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/users/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8080/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ login, password }),
       });
 
       if (response.ok) {
         const data = await response.json();
+        localStorage.setItem("userId", data.id);
+        localStorage.setItem("clinicId", data.clinics_id.id);
         console.log(data.clinics_id);
-        navigate('/schedule');
+        navigate("/schedule");
       } else {
         setError("Login failed. Please check your credentials.");
       }
@@ -36,14 +38,41 @@ export default function Login() {
     <div className="flex h-screen p-8 bg-green-500 gap-5 items-center justify-center">
       <div className="bg-white w-5/12 p-6 rounded-md">
         <div className="flex flex-1 flex-col h-full items-center justify-center gap-8">
-            <img src="src\\assets\\logotipo.png" style={{width: 200, height: 100, objectFit: 'cover', borderRadius: 50}} />
-            <InputComponent label="Login" id="login" name="login" onChange={(e: { target: { value: SetStateAction<string>; }; }) => setLogin(e.target.value)} values={login} />
-            <InputComponent label="Senha" id="password" name="password" onChange={(e: { target: { value: SetStateAction<string>; }; }) => setPassword(e.target.value)} values={password} />
-            {error && <div style={{color: 'red'}}>{error}</div>}
-            <div className="w-full flex flex-1 items-center justify-center gap-8">
-                <SimpleButton label="Entrar" onClick={handleLogin}/>
-                <SimpleButton label="Cadastrar" onClick={() => navigate('/sign-up')}/>
-            </div>
+          <img
+            src="src\\assets\\logotipo.png"
+            style={{
+              width: 200,
+              height: 100,
+              objectFit: "cover",
+              borderRadius: 50,
+            }}
+          />
+          <InputComponent
+            label="Login"
+            id="login"
+            name="login"
+            onChange={(e: { target: { value: SetStateAction<string> } }) =>
+              setLogin(e.target.value)
+            }
+            values={login}
+          />
+          <InputComponent
+            label="Senha"
+            id="password"
+            name="password"
+            onChange={(e: { target: { value: SetStateAction<string> } }) =>
+              setPassword(e.target.value)
+            }
+            values={password}
+          />
+          {error && <div style={{ color: "red" }}>{error}</div>}
+          <div className="w-full flex flex-1 items-center justify-center gap-8">
+            <SimpleButton label="Entrar" onClick={handleLogin} />
+            <SimpleButton
+              label="Cadastrar"
+              onClick={() => navigate("/sign-up")}
+            />
+          </div>
         </div>
       </div>
     </div>
