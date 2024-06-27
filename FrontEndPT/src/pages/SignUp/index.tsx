75fb23e6339e3e228d202714be0297f0b9e4965f
phillCD <132512214+phillCD@users.noreply.github.com>
2024-06-27 17:47:41 -0300

    main: clinic cadastro

--- FrontEndPT/src/pages/SignUp/index.tsx
+++ FrontEndPT/src/pages/SignUp/index.tsx
@@ -15,6 +15,13 @@ const SignUp = () => {
   const [speciality, setEspecialidade] = useState('');
   const [password, setSenha] = useState('');
   const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [clinics, setClinics] = useState<Clinics[]>([]);
+  const [selectedClinic, setSelectedClinic] = useState<Clinics | null>(null);
+  interface Clinics {
+    id: number;
+    name: string;
+    address: string;
+  }
   const [error, setError] = useState('');
 
 
@@ -96,6 +103,7 @@ const SignUp = () => {
       address: address,
       speciality: speciality,
       password: password,
+      clinics_id: selectedClinic,
     };
   
     try {
@@ -115,6 +123,23 @@ const SignUp = () => {
       console.log('SignUp successful:', data);
     } catch (error) {
       console.error('SignUp failed:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:8080/clinic')
+      .then(response => response.json())
+      .then(data => {
+        setClinics(data);
+      })
+      .catch(error => console.error('Error fetching clinic:', error));
+  }, []);
+
+  const handleClinicChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const clinicId = parseInt(event.target.value, 10);
+    const clinic = clinics.find(c => c.id === clinicId);
+    if (clinic) {
+      setSelectedClinic(clinic);
     }
   };
 
@@ -142,6 +167,21 @@ const SignUp = () => {
                     <option value="MASCULINO">Masculino</option>
                     <option value="FEMININO">Feminino</option>
                     <option value="OUTRO">Outro</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="">Clinica</label>
+                  <select
+                    className="form-select block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    value={selectedClinic ? selectedClinic.id : ''}
+                    onChange={handleClinicChange}
+                  >
+                    <option value="">Selecione</option>
+                    {clinics.map((clinic) => (
+                      <option key={clinic.id} value={clinic.id}>
+                        {clinic.name}
+                      </option>
+                    ))}
                   </select>
                 </div>
                 <InputComponent label="Celular" id={"cellphone"} name={"cellphone"} onChange={(e) => setCelular(e.target.value)} values={cellphone} />
